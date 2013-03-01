5cfcbb74d00ccbb257c1e561f5601541ee2c0fca
jean pierre <yvens47@yahoo.com>
2013-03-01 14:48:21 -0500

    create a scaffoling for story book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storybook::Application.config.secret_token = '1c763318cfd5eec9853ea375b69864071c891297283361f8d723a2e92b08edd5b77a5145f0a1571cbd3da08697eaa8fcc4f6ece49ccda3db7573d0318b284e9d'
