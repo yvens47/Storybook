83ca674c2475b140674b35ece5de190dd582be9e
jean pierre <yvens47@yahoo.com>
2013-04-03 12:29:09 -0400

    add omiauth callbacks

---
+++ app/controllers/omniauth_callback_controller.rb
@@ -0,0 +1,7 @@
+class OmniauthCallbackController < ApplicationController
+  def passthru
+  render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  # Or alternatively,
+  # raise ActionController::RoutingError.new('Not Found')
+end
+end
