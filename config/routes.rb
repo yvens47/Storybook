e9be8b374eb9857cb8fa54ab3671ab0c82f9cf4f
jean pierre <yvens47@yahoo.com>
2013-04-04 20:09:38 -0400

    add facebook link

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Storybook::Application.routes.draw do
 
   get "home/index"
 
-  devise_for :users
+  devise_for :users,:controllers => { :omniauth_callbacks => "/omniauth_callbacks" }
   devise_scope :user do
   get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
 end
