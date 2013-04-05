5e5922c09b3a4fe73c4aeb105b450e04cedf5bf5
jean pierre <yvens47@yahoo.com>
2013-04-04 20:18:43 -0400

    add omniauth to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,17 @@ class User < ActiveRecord::Base
   def full_name
     first_name + " "+last_name
   end
+  
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  unless user
+    user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+                         )
+  end
+  user
+end
 end
